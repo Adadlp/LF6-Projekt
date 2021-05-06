947a8bd2b0bf7a55a34721f79432649cf0d4b97b
Adadlp <83219156+Adadlp@users.noreply.github.com>
2021-05-06 12:38:33 +0200

    ICON und Fendster design

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ import os
 import tkinter as tk
 from tkinter.filedialog import askopenfilename
 
-def Wortliste():
+def wortliste():
     global wort_liste
     x = os.environ['USERPROFILE'] + "\Desktop"
     wort_liste = askopenfilename(initialdir= x, filetypes =(("Text File", "*.txt"),("All Files","*.*")), title = "Wähle die Passwortliste aus.")
+    pflabel.configure(text = wort_liste)
+
+def hintergrund():
+    None
 
 class Pw:
     def getpw(Self):
@@ -50,23 +54,60 @@ class Pw:
 #Wichtig
 wort_liste = ""
 o = Pw()
+hintergd = '#FFFFFF'
+
 #Fenster
 fenster = tk.Tk()
+icon = tk.PhotoImage(file = 'ICON.png')
 fenster.title("     Hasher / Dehasher")
 fenster.geometry("1280x720")
-fenster.iconphoto()
-button1 = tk.Button(master = fenster, bg = "light grey", relief = "groove", text = "Wortliste \n auswählen", command = Wortliste)
-button1.place(x = 100, y = 620, width = 80, height = 80)
-button1.pack
+fenster.iconphoto(False, icon)
+fenster.configure(bg = hintergd)
+#wichtig 2
+
+# Hash oder dehash
+button1p1 = tk.Button(master = fenster, bg = "light grey", relief = "groove", text = "Wortliste \n auswählen", command = wortliste)
+button1p1.place(x = 100, y = 620, width = 80, height = 80)
+button1p1.pack
+
+button1p2 = tk.Button(master = fenster, bg = "light grey", relief = "groove", text = "Wortliste \n auswählen", command = wortliste)
+button1p2.place(x = 100, y = 620, width = 80, height = 80)
+button1p2.pack
 #Wortliste auswählen
-button2 = tk.Button(master = fenster, bg = "light gray", relief = "groove", text = "Wortliste \n auswählen", command = Wortliste)
-button2.place(x = 100, y = 620, width = 80, height = 80)
+button2 = tk.Button(master = fenster, bg = "light gray", relief = "groove", text = "Wortliste auswählen", command = wortliste)
+button2.place(x = 840, y = 70, width = 400, height = 40)
 button2.pack
+
 #Passwort eingabe feld
 eingabefeld = tk.Entry(master = fenster,show = "*")
 eingabefeld.place(x = 100, y = 220, width = 200, height = 20)
 eingabefeld.pack
 #Ausgabe
 ausgabelabel = tk.Label(master= fenster, )
+
+#Einstellungen
+einstelllabel = tk.Label(master = fenster, bg = hintergd, text = "Einstellungen")
+einstelllabel.place(x = 840, y = 20, width = 400, height = 40)
+einstelllabel.config(font = ("Arial", 18))
+einstelllabel.pack
+#Pfad zur Wortliste
+plabel = tk.Label(master = fenster, bg = hintergd, text = "Pfad zur derzeit angewendeten Wortliste:")
+plabel.place(x = 840, y = 530, width = 400, height = 40)
+plabel.config(font = ('Arial', 15))
+plabel.pack
+
+pflabel = tk.Label(master = fenster, bg = hintergd)
+pflabel.place(x = 840, y = 570, width = 400, height = 40)
+pflabel.config(font = ('Arial', 12))
+pflabel.pack
+
+#Hintergrundfarbe auswählen
+button3 = tk.Button(master = fenster, bg = "light gray", relief = "groove", text = "Hintergrundfarbe auswählen", command = hintergrund)
+button3.place(x = 840, y = 130, width = 400, height = 40)
+button3.pack
+#Linie
+#draw = tk.Canvas(master = fenster)
+#draw.create_line(800, 0, 800, 180, width = 2)
+#draw.pack()
 fenster.mainloop()
 
